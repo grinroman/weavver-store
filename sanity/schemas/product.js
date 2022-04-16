a8e405d268c54bc1dcbb5782c5c4690da0490f7b
Roman Grin <grinromchik@gmail.com>
2022-04-16 14:35:24 +0300

    FULLSTACK: added sanity template, added product schema

---
+++ sanity/schemas/product.js
@@ -0,0 +1,75 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'sale',
+            title: 'Sale',
+            type: 'number',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true, //define the most important type of the image
+            },
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'slug', //жетон - в него конвертируется наше название
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'brand',
+            title: 'Brand',
+            type: 'string',
+        },
+        {
+            name: 'size',
+            title: 'Size',
+            type: 'string',
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+        },
+        {
+            name: 'rating',
+            title: 'Rating',
+            type: 'number',
+        },
+
+        {
+            name: 'numReviews',
+            title: 'NumReviews',
+            type: 'number',
+        },
+        {
+            name: 'countInStock',
+            title: 'CountInStock',
+            type: 'number',
+        },
+    ],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,11 @@
 import type { AppProps } from 'next/app';
 import 'src/scss/index.scss';
 import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
 
 const clientSideEmotionCache = createCache({ key: 'css' });
 
-function MyApp({
-    Component,
-    pageProps,
-    emotionCache = clientSideEmotionCache,
-}: any) {
-    return (
-        <CacheProvider value={emotionCache}>
-            <Component {...pageProps} />
-        </CacheProvider>
-    );
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
